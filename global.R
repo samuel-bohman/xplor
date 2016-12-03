e1ef1d5002ede62d09aeda937153ca1df0d3008d
samuel-bohman <samuel.bohman@yahoo.com>
2016-12-03 20:43:24 +0100

    Changed the basic layout of the app with shinydashboard(). Adapted parts of server.R and global.R to the new layout. Some additional minor changes.

--- global.R
+++ global.R
@@ -1,91 +1,60 @@
-# Load libraries
 library(shiny)
+library(shinydashboard)
 library(leaflet)
 library(sp)
 library(RColorBrewer)
 
-# Read data ----
-# SpatialPolygonsDataFrame for group 1
-results_spdf1 <- readRDS("data/results2.rds") 
-# SpatialPolygonsDataFrame for group 2
-results_spdf2 <- readRDS("data/results2.rds") 
-# DataFrame for tabPanel "Data"
-results_df <- read.table(
-    "data/results.csv",
-    header = TRUE,
-    sep = ";",
-    fileEncoding = "UTF-8"
-  ) 
-# SpatialPolygonsDataFrame for nyko polygons
+# SpatialPolygonsDataFrame for map polygons
 nyko <- readRDS("data/nyko.rds") 
 
-# Define background variables ----
-vars_area <-
-  c("Inget val" = "", "Alla", sort(unique(as.character(results_df[["Omrade"]]))))
-vars_sex <-
-  c("Inget val" = "",
-    "Alla",
-    "Kvinna",
-    "Man",
-    "Vill ej uppge",
-    "Annat/inget kön")
-vars_age <-
-  c("Inget val" = "", "Alla", sort(unique(as.character(results_df[["Ålder"]]))))
-vars_occupation <-
-  c("Inget val" = "", "Alla", sort(unique(as.character(results_df[["Sysselsättning"]]))))
-vars_education <-
-  c(
-    "Inget val" = "",
-    "Alla",
-    "Inte gått ut grundskola eller motsvarande obligatorisk skola",
-    "Grundskola eller motsvarande obligatorisk skola",
-    "Gymnasium, folkhögskola eller motsvarande",
-    "Annan eftergymnasial utbildning",
-    "Högskola/universitet",
-    "Forskarutbildning"
-  )
+# SpatialPolygonsDataFrame for group 1
+results_spdf1 <- readRDS("data/results_spdf.rds")
 
-vars_years <- c("Inget val" = "", "Alla", "0-4 år", "5-9 år", "10 år eller mer")
+# SpatialPolygonsDataFrame for group 2
+results_spdf2 <- readRDS("data/results_spdf.rds")
+
+# data frame for tabPanel "Data"
+results_df <- read.table("data/results.csv", header = TRUE, sep = ";", fileEncoding = "UTF-8")
+
+# Define background variables ----
+vars_area <- c("No selection" = "", "All", sort(unique(as.character(results_df[["Area"]]))))
+vars_sex <- c("No selection" = "", "All", "Woman", "Man", "Prefer not to disclose", "Other/No gender")
+vars_age <- c("No selection" = "", "All", sort(unique(as.character(results_df[["Age"]]))))
+vars_occupation <- c("No selection" = "", "All", sort(unique(as.character(results_df[["Occupation"]]))))
+vars_education <- c("No selection" = "", "All", sort(unique(as.character(results_df[["Education.level"]]))))
+vars_years <- c("No selection" = "", "All", "0-4 years", "5-9 years", "10 years or more")
 
 # Needed for dynamic form generation (group forms)
 background_choices <- list(vars_area, vars_sex, vars_age, vars_occupation, vars_education, vars_years)
 
 # Use these for input matching - TODO: Generate dynamically / From file
 # Names to be used as basis for group input dropdown ids
-ui_names_bg <- c("area", "sex", "age", "occupation", "education", "years")
+ui_names_bg <- c("area", "gender", "age", "occupation", "education", "years")
 
 # Corresponding dataframe column names
-df_names_bg <- c("Omrade", "Kön", "Ålder", "Sysselsättning", "Utbildningsnivå", "År")
+df_names_bg <- c("Area", "Gender", "Age", "Occupation", "Education.level", "Year")
 
 # The labels that will appear for the ui_names entries on the forms
-dropdown_names_bg <- c("Område", "Kön", "Ålder", "Sysselsättning", "Utbildningsnivå", "Hur länge bott i kommunen")
+dropdown_names_bg <- c("Area", "Gender", "Age", "Occupation", "Education", "Length of residency")
 names(df_names_bg) <- ui_names_bg
 
 # Determines the number of controlled groups (generates a form for each)
-group_amount = 2
+# group_amount = 2
 
 # Define themes and alternatives variables ----
-themes <- c("1. Parker & grönområden", "2. Mångfald i bostadsutbudet", "3. Levandegöra gemensamma platser", "4. Kommunikationer", "5. Kultur & fritid", "6. Utbildning", "7. Omsorg", "8. Skolan", "9. Trygghet", "10. Hållbar utveckling")
-alt_theme_1 <- c("1a. Bevara existerande större grönområden", "1b. Anlägga parker i existerande stadsdelar", "1c. Bygga bostäder nära grönområden", "1d. Rusta upp befintliga parker", "1e. Skapa bättre tillgänglighet till större grönområden")
-alt_theme_2 <- c("2a. Erbjuda fler bostadstyper", "2b. Erbjuda fler lägenhetsstorlekar", "2c. Erbjuda småskaligt markägande", "2d. Bevara de idémässiga grunderna för bebyggelsen från 1970-talet", "2e. Erbjuda fler bostäder nära vatten")
-alt_theme_3 <- c("3a. Blanda trafikslagen", "3b. Förlägga parkering längs med gator", "3c. Vända entréer mot gator", "3d. Förlägga publika lokaler i transparenta bottenvåningar", "3e. Trygga parkeringslösningar under bostäder")
-alt_theme_4 <- c("4a. Koppla ihop nya gator med befintliga för att stärka kopplingen till intilliggande stadsdelar och minska barriärerna som de stora vägarna utgör", "4b. Förbättra kommunikationerna nattetid mellan olika delar av kommunen", "4c. Förbättra kommunikationerna till och från Uppsala", "4d. Förbättra de nord-sydliga och öst-västliga stråken via ett finmaskigare och väl integrerat stadsnät", "4e. Förbättra kommunikationerna till och från Stockholms innerstad")
-alt_theme_5 <- c("5a. Utöka utbudet av kultur- sport- och fritidsaktiviteter", "5b. Skapa bättre möjligheter för festivaler och konserter", "5c. Skapa fler förutsättningar för utomhussporter", "5d. Skapa marknadsplatser utomhus", "5e. Erbjuda kommunala bidrag för kultur- och fritidsprojekt")
-alt_theme_6 <- c("6a. Rusta upp äldre skolor", "6b. Bygg nya skolor", "6c. Förbättra skolgårdarnas fysiska miljöer", "6d. Höj kvaliteten i grundskolan", "6e. Höj kvaliteten på gymnasieutbildningarna")
-alt_theme_7 <- c("7a. Fler kultur- och fritidsaktiviteter för äldre", "7b. Fler kultur- och fritidsaktiviteter för barn och ungdomar", "7c. Förbättra äldreomsorgen i kommunen", "7d. Fler ungdomsgårdar och fältassistenter", "7e. Minska barngrupperna i förskolan")
-alt_theme_8 <- c("8a. Mindre barngrupper i förskolan", "8b. Höj kvaliteten i undervisningen", "8c. Mer kompetensutveckling för skolor och lärare", "8d. Mer modern informationsteknologi (IT) i undervisningen", "8e. Involvera vårdnadshavare mer i skolan")
-alt_theme_9 <- c("9a. Öka tryggheten kring stationsområdet", "9b. Fler poliser i centrala Väsby", "9c. Förbättra belysningen i centrala Väsby", "9d. Begränsa öppettider för alkoholutskänkning i centrala Väsby", "9e. Förläng öppettider för affärsverksamhet i centrala Väsby")
-alt_theme_10 <- c("10a. Minska förbrukningen av energi", "10b. Minska transporter och buller", "10c. Öka klimatanpassning och kretsloppstänkande", "10d. Prioritera miljövänliga transportsätt (gång, cykel, kollektivtrafik)", "10e. Minska miljögifter och farliga kemikalier i naturen")
-alt_list <-
-  list(
-    alt_theme_1,
-    alt_theme_2,
-    alt_theme_3,
-    alt_theme_4,
-    alt_theme_5,
-    alt_theme_6,
-    alt_theme_7,
-    alt_theme_8,
-    alt_theme_9,
-    alt_theme_10
-  )
+themes <- colnames(results_df)[57:66]
+themes <- gsub(pattern = ".", replacement = " ", x = themes, fixed = TRUE)
+themes <- paste(c(1:10), themes, sep = ". ")
+
+
+alt_theme_1 <- c("1a. Preserve existing large green areas", "1b. Build parks in existing urban districts", "1c. Build homes close to green areas", "1d. Renovate existing parks", "1e. Improve accessibility to major green areas")
+alt_theme_2 <- c("2a. Offer more residential building types", "2b. Offer more apartment sizes", "2c. Offer small-scale land ownership", "2d. Preserve the conceptual foundations of the buildings from the 1970s", "2e. Offer more waterfront residences")
+alt_theme_3 <- c("3a. Enable more diverse traffic", "3b. Enable car parking along the streets", "3c. Face residential entrances toward the streets", "3d. Make public ground floor premises transparent", "3e. Build underground car parks in residential buildings")
+alt_theme_4 <- c("4a. Pair new streets with existing ones to strengthen the connection to adjacent neighborhoods and to reduce the barriers that the large roads pose", "4b. Improve late night public transport", "4c. Improve public transport to and from Uppsala", "4d. Improve the north-south and east-west routes through a fine-mesh transportation network", "4e. Improve public transport to and from Stockholm")
+alt_theme_5 <- c("5a. Expand the range of cultural, sporting, and recreational activities", "5b. Create better opportunities for festivals and concerts", "5c. Create better opportunities for outdoor recreation", "5d. Organize public and farmers' markets", "5e. Provide municipal grants for cultural and recreational projects")
+alt_theme_6 <- c("6a. Renovate old schools", "6b. Build new schools", "6c. Refurbish school yards", "6d. Improve the education in primary schools", "6e. Improve the education in high schools")
+alt_theme_7 <- c("7a. More cultural and recreational activities for the elderly", "7b. More cultural and recreational activities for children and young people", "7c. Improve care for the elderly", "7d. More youth centres and field assistants", "7e. Reduce preschool child groups")
+alt_theme_8 <- c("8a. Reduce preschool child groups", "8b. Raise the quality of teaching", "8c. More professional development for school teachers", "8d. More modern information technology (IT) in education", "8e. Involve caretakers more in school")
+alt_theme_9 <- c("9a. Increase safety around the train station", "9b. More police officers in the city center", "9c. Improve the lighting in the city center", "9d. Restrict the opening hours of bars and restaurants who serve alcohol in the city centre", "9e. Extend the opening hours of shops in the city center")
+alt_theme_10 <- c("10a. Reduce energy consumption", "10b. Reduce transport noise and sound pollution", "10c. Increase climate adaptation and recycling", "10d. Prioritize environmentally friendly transport modes (walking, cycling, public transport)", "10e. Reducing environmental toxins and hazardous chemicals in nature")
+alt_list <- list(alt_theme_1, alt_theme_2, alt_theme_3, alt_theme_4, alt_theme_5, alt_theme_6, alt_theme_7, alt_theme_8, alt_theme_9, alt_theme_10)
