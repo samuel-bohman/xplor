7f0fd43011eca7818c7f688140a84d97d05deef7
Tobias <fasth@dhcp-8-3-249.univ-tlse1.fr>
2017-04-26 15:21:08 +0200

    Updates sd.R with mean values.

--- sd.R
+++ sd.R
@@ -12,21 +12,33 @@
     alternatives <- get_alternatives_sd(criterion)
     alternatives_cop <- c(paste(alternatives, ".cop", sep = ""))
     alternatives_val <- c(paste(alternatives, ".val", sep = ""))
-    alternatives_con <- c(paste(alternatives, ".con", sep = ""))
-    alternatives_pro <- c(paste(alternatives, ".pro", sep = ""))
+    alternatives_sd <- c(paste(alternatives, ".sd", sep = ""))
+    alternatives_con_sd <- c(paste(alternatives, ".con_sd", sep = ""))
+    alternatives_pro_sd <- c(paste(alternatives, ".pro_sd", sep = ""))
+    alternatives_mean <- c(paste(alternatives, ".mean", sep = ""))
+    alternatives_con_mean <- c(paste(alternatives, ".con_mean", sep = ""))
+    alternatives_pro_mean <- c(paste(alternatives, ".pro_mean", sep = ""))
     result_names <- c()
     result_values <- c()
     tmp_results <- c()
     tmp_names <- c()
     
     for (i in 1:(length(alternatives))) {
-      result_names <- c(result_names, c(alternatives_con[i], alternatives_pro[i], alternatives_val[i]))
+      
+      result_names <- c(result_names, c(alternatives_mean[i], alternatives_con_mean[i], alternatives_pro_mean[i], alternatives_sd[i], alternatives_con_sd[i], alternatives_pro_sd[i]))
+      
       results_con <- subset(results, eval(parse(text = paste0(alternatives_cop[i]))) == "0")
       results_pro <- subset(results, eval(parse(text = paste0(alternatives_cop[i]))) == "1")
+      
+      results_mean <- mean(unlist(results[alternatives_val[i]]))
+      results_mean_con <- mean(unlist(results_con[alternatives_val[i]]))
+      results_mean_pro <- mean(unlist(results_pro[alternatives_val[i]]))
+      
+      results_sd <- sd(unlist(results[alternatives_val[i]]))
       results_sd_con <- sd(unlist(results_con[alternatives_val[i]]))
       results_sd_pro <- sd(unlist(results_pro[alternatives_val[i]]))
-      results_sd <- sd(unlist(results[alternatives_val[i]]))
-      result_values <- c(result_values, c(respults_sd_con, results_sd_pro, results_sd))
+
+      result_values <- c(result_values, c(results_mean, results_mean_con, results_mean_pro, results_sd, results_sd_con, results_sd_pro))
     }
     
     names(result_values) <- result_names
