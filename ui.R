0a5ae0a308112c22ef438b29decd445fa1d3a1da
samuel-bohman <samuel.bohman@yahoo.com>
2017-08-28 21:19:27 +0200

    Commit

--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ T    <- HTML("<font color='darkslateblue'><b>T</b></font>")
 Diff <- HTML("<font color='darkslateblue'><b>Diff</b></font>")
 
 shinyUI(
-  navbarPage(title = HTML("<font color='#4080bf'><b>Upplands Väsby Data Explorer</b></font>"), id = "nav", position = "static-top", collapsible = TRUE, fluid = TRUE, 
+  navbarPage(title = HTML("<font color='#4080bf'><strong>Upplands Väsby Data Explorer</strong></font>"), id = "nav", position = "static-top", collapsible = TRUE, fluid = TRUE, 
     tabPanel(title = "Map", icon = icon(name = "map-o"),
       fluidPage(
         fluidRow(
