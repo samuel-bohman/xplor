55d8f63bd43485759a0de510f6904c9eca45c575
Tobias <fasth@dsv.su.se>
2018-02-14 10:33:18 +0100

    Bug fix.

--- r/distance.R
+++ r/distance.R
@@ -42,7 +42,7 @@ distance <- function(criterion, spdf) {
     # Con mean
     mean_namesc <- c(mean_names, c(alternatives_cmean[i]))
     mean_valuesc <- c(mean_valuesc, mean(data[data[alternatives_cop[i]] == 0,][,alternatives_cval[i]]))
-
+    
     # Pro mean
     mean_namesp <- c(mean_namesp, c(alternatives_pmean[i]))
     mean_valuesp <- c(mean_valuesp, mean(data[data[alternatives_cop[i]] == 1,][,alternatives_pval[i]]))
@@ -56,19 +56,19 @@ distance <- function(criterion, spdf) {
         } else {0}
       })
     
-      data[alternatives_pvar[i]] <-
+    data[alternatives_pvar[i]] <-
       apply(data, 1, function(x) {
         if (as.numeric(x[alternatives_cop[i]]) == 1) {
           pvar <- ((as.numeric(x[alternatives_pval[i]]) - as.numeric(mean_valuesp[i]))^2)*lambda
         } else {0}
       })
     
-      data[alternatives_var[i]] <-
-        apply(data, 1, function(x) {
-          var <- ((as.numeric(x[alternatives_val[i]]) - as.numeric(mean_values[i]))^2)*lambda
-        }) 
+    data[alternatives_var[i]] <-
+      apply(data, 1, function(x) {
+        var <- ((as.numeric(x[alternatives_val[i]]) - as.numeric(mean_values[i]))^2)*lambda
+      }) 
   }
-    # prepare results
+  # prepare results
   for (i in 1:(length(alternatives) - 1)) {
     
     # Summarize nr, con, and pro 
@@ -79,7 +79,7 @@ distance <- function(criterion, spdf) {
     tmp_names <- c(tmp_names, c(alternatives_nr[i]))
     tmp_data <- c(tmp_data, c(sum(sum(data[, alternatives_cop[i]] == 0), sum(data[, alternatives_cop[i]] == 1))))
     names(tmp_data) <- tmp_names
-
+    
     # result_values is the distance between the pro and con groups for alternative i.
     result_names <- c(result_names, c(alternatives_var[i]))
     result_values <- c(result_values, c(abs(sum(data[, alternatives_cvar[i]])+sum(data[, alternatives_pvar[i]])-sum(data[, alternatives_var[i]]))))
@@ -87,19 +87,19 @@ distance <- function(criterion, spdf) {
     # Con index
     result_names <- c(result_names, c(alternatives_cval[i]))
     result_values <- c(result_values, c(sum(data[, alternatives_cval[i]]) * lambda))
-
+    
     # Pro index
     result_names <- c(result_names, c(alternatives_pval[i]))
     result_values <- c(result_values, c(sum(data[, alternatives_pval[i]]) * lambda))
-     
+    
     # Avg value
     result_names <- c(result_names, c(alternatives_val[i]))
     result_values <- c(result_values, c(sum(data[, alternatives_val[i]])) * lambda)
- 
+    
     # Number of members of the con group
     result_names <- c(result_names, c(alternatives_nr_con[i]))
     result_values <- c(result_values, c(tmp_data[alternatives_nr_con[i]]))
-     
+    
     # Number of members of the pro group
     result_names <- c(result_names, c(alternatives_nr_pro[i]))
     result_values <- c(result_values, c(tmp_data[alternatives_nr_pro[i]]))
